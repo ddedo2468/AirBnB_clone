65826ec479e28c9dd4478576d9e5789301737945
Abdullah Musbah <58207352+ddedo2468@users.noreply.github.com>
2023-11-12 05:50:57 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,48 @@
 #!/usr/bin/python3
-""" defining the BaseModel and its correspondent methods """
 
+"""BaseModel the parent class """
+from uuid import uuid4
 import models
 from datetime import datetime
-from uuid import uuid4
 
 
 class BaseModel:
-""" Base Class """
-   def __init__(self, *args, **kwargs):
-        """ initializing object """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+    """Define BaseModel class"""
+
+    def __init__(self, *args, **kwargs):
+        """the class constructor """
+
+        format_time = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, time_format)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, format_time)
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = value
         else:
             models.storage.new(self)
 
     def __str__(self):
-    """ printing the string representation """
-      new = (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-       return new
+        """Print strint repr for the objs """
+
+        new_str = ("[{}] ({}) {}".format(self.__class__.__name__,
+                                         self.id, self.__dict__))
+        return new_str
 
     def save(self):
-    """ updating the object with the latest time """
-      self.updated_at = datetime.today()
-       models.storage.save()
+        """Update the last time obj edited at """
+
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-    """ returns the dictionary of the BaseModel instances """
-      new_dict = self.__dict__.copy()
-       new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict["__class__"] = self.__class__.__name__
-        return new_dict
+        """return a dictionary of baseModel instances"""
+
+        nw_dictionary = self.__dict__.copy()
+        nw_dictionary["created_at"] = self.created_at.isoformat()
+        nw_dictionary["updated_at"] = self.updated_at.isoformat()
+        nw_dictionary["__class__"] = self.__class__.__name__
+        return nw_dictionary
